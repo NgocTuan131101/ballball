89ee3b85ffb8c3c9fe7c4e1bcf3db6e69740dcf1
NgocTuan01 <106262125+NgocTuan01@users.noreply.github.com>
2023-06-15 15:08:48 +0700

    1506

--- lib/src/presentation/onboadrding/onboarding_Controller.dart
+++ lib/src/presentation/onboadrding/onboarding_Controller.dart
@@ -6,13 +6,23 @@ import 'package:get/utils.dart';
 class OnboardingController extends GetxController {
   var selectedPageeIndex = 0.obs;
 
+
   bool get isLastPage => selectedPageeIndex.value == OnboadrdingPage.length - 1;
   var pageController = PageController();
 
+
+  // chuyển tiếp trang tiếp theo
   forwarAction() {
-    if (isLastPage) {
+    // nếu true tức là đang ở  trnag cuối cùng
+    if (isLastPage)  {
     } else
+      // chuyển tới trang tiếp theo bà chuyển đổi 300
       pageController.nextPage(duration: 300.milliseconds, curve: Curves.ease);
+  }
+  backAction() {
+    if (selectedPageeIndex.value > 0) {
+      pageController.previousPage(duration: 300.milliseconds, curve: Curves.ease);
+    }
   }
 
   List<OnboardingInfo> OnboadrdingPage = [
--- lib/src/presentation/onboadrding/onboarding_page.dart
+++ lib/src/presentation/onboadrding/onboarding_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:BallBall/src/presentation/onboadrding/onboarding_Controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +9,7 @@ class OnboadrdingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: Container(
         child: Stack(
           children: [
             PageView.builder(
@@ -44,15 +43,37 @@ class OnboadrdingPage extends StatelessWidget {
                   );
                 }),
             Positioned(
+              top: 30,
+              left: 0,
               right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  _controller.OnboadrdingPage.length,
+                  (index) => Obx(() {
+                    return Container(
+                      margin: const EdgeInsets.all(4),
+                      width: 12,
+                      height: 12,
+                      decoration: BoxDecoration(
+                          color: _controller.selectedPageeIndex.value == index
+                              ? Colors.green
+                              : Colors.white,
+                          shape: BoxShape.circle),
+                    );
+                  }),
+                ),
+              ),
+            ),
+            Positioned(
+              right: 4,
               top: 16,
               child: Column(
                 children: [
                   Container(
                       alignment: Alignment.centerRight,
                       child: TextButton(
-                        onPressed: () {
-                        },
+                        onPressed: () {},
                         child: const Text(
                           'Bỏ qua',
                           style: TextStyle(
@@ -65,34 +86,34 @@ class OnboadrdingPage extends StatelessWidget {
               ),
             ),
             Positioned(
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(
-                    _controller.OnboadrdingPage.length,
-                    (index) => Obx(() {
-                      return Container(
-                        margin: const EdgeInsets.all(4),
-                        width: 12,
-                        height: 12,
-                        decoration: BoxDecoration(
-                            color: _controller.selectedPageeIndex.value == index
-                                ? Colors.green
-                                : Colors.white,
-                            shape: BoxShape.circle),
-                      );
-                    }),
-                  ),
+              left: 4,
+              top: 16,
+              child: TextButton.icon(
+                onPressed: () {
+                  _controller.backAction();
+                },
+                style: ElevatedButton.styleFrom(),
+                icon: const Icon(
+                  Icons.arrow_back_ios_outlined,
+                  color: Colors.white,
+                  size: 18,
+                ),
+                label: const Text(
+                  "Quay lại",
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
               ),
             ),
             Positioned(
               bottom: 60,
               right: 28,
               child: InkWell(
-                onTap: (){
+                onTap: () {
                   _controller.forwarAction();
                 },
                 child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 15.0, vertical: 10.0),
                   decoration: BoxDecoration(
                     color: Colors.green,
                     borderRadius: BorderRadius.circular(8.0),
@@ -102,12 +123,12 @@ class OnboadrdingPage extends StatelessWidget {
                     children: const [
                       Text(
                         'Tiếp tục',
-                        style: TextStyle(fontSize: 16,
-                        color : Colors.black54
-                        ),
+                        style: TextStyle(fontSize: 16, color: Colors.black54),
                       ),
                       SizedBox(width: 0.0),
-                      Icon(Icons.navigate_next,color: Colors.black54,
+                      Icon(
+                        Icons.navigate_next,
+                        color: Colors.black54,
                       ),
                     ],
                   ),
